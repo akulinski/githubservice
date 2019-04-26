35d78a445f185e0bba1a0a0fddff21c39924c354
albert <albert.kulinski@hotmail.com>
2019-04-26 18:30:37 +0200

    Initial commit

---
+++ src/main/java/com/akulinski/githubservice/GithubserviceApplication.java
@@ -0,0 +1,13 @@
+package com.akulinski.githubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubserviceApplication.class, args);
+    }
+
+}
